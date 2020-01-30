a2d3b3c4a8adc7dc06c8d159f14ab99113232b7a
JackMao981 <yuxiangmao981@gmail.com>
2020-01-30 18:28:38 -0500

    My understanding of optimization

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    // The line that corresponds to x = 5 is:
+    // movl	$5, -4(%rbp)
+    // If optimization is turned on, the line int x = 5 which is never used anywhere else,
+    // is not in the assembly code
     int x = 5;
-    printf("Hello, World!\n");
+    int y = x + 1;
+    printf("%i",y);
     return 0;
+    // From what I experienced, optimization ignores values that are only declared and not used.
+    // However, if a declaration refers to another declared variable, then both are retained.
 }
